8100f0904aed2535f5678a5563b966666323b90b
X230\nadhi <nadhifyondar@gmail.com>
2018-06-25 14:56:33 +0900

    Progress: - Penambahan sistem scoring - Penambahan Game Feel
    Kendala:
    - Game belum dicoba terkendala oleh device

---
+++ Assets/Scripts/basketball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class basketball : MonoBehaviour {
+
+	public Text score;
+	public Text remainingballs;
+
+	private int currentscore = 0;
+	public int remainingballs_number = 3;
+	private Vector3 InitialPosition;
+
+	void Start()
+	{
+		InitialPosition = this.transform.position;
+		ChangeText ();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Ring") 
+		{
+			ScoreUpdate ();
+			remainingballs_number++;
+		}
+	}
+
+	public void DecreaseRemainingBalls()
+	{
+		remainingballs_number--;
+	}
+
+	public void ChangeText()
+	{
+		remainingballs.text = "Remaining Balls: " + remainingballs_number.ToString ();
+	}
+
+	private void ScoreUpdate(){
+		currentscore++;
+		score.text = currentscore.ToString ();
+	}
+
+	public void ResetPosition()
+	{
+		this.transform.position = InitialPosition;
+		this.GetComponent<Rigidbody>().useGravity = false;
+		this.GetComponent<Rigidbody>().isKinematic = true;
+		this.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+	}
+}
---
+++ Assets/Scripts/boundary.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundary : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+	{
+		col.GetComponent<basketball> ().ResetPosition ();
+		col.GetComponent<basketball> ().DecreaseRemainingBalls ();
+		col.GetComponent<basketball> ().ChangeText ();
+	}
+
+}
